2f45d2cc9b505332f07c54be096f71d07c9e61d4
Chris Garvey <chrisgarvey@chris-garveys-imac.local>
2015-11-17 08:19:33 -0800

    add @IBActions for all four toolbar items

--- MemeMe-1.0/ViewController.swift
+++ MemeMe-1.0/ViewController.swift
@@ -20,9 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Hide the status bar during app use
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
 
+    @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func pickImage(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func cancelAction(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func shareMeme(sender: UIBarButtonItem) {
+    }
+    
 }
 
