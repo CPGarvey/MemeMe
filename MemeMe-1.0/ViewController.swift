958d68a3b3a517f22614ebb771cef45d82589db5
Chris Garvey <chrisgarvey@chris-garveys-imac.local>
2015-11-18 21:22:35 -0800

    add Image View, Outlet for that Image View, and delegate method to send a selected image to that Image View

--- MemeMe-1.0/ViewController.swift
+++ MemeMe-1.0/ViewController.swift
@@ -11,15 +11,17 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Disable camera if device does not have a camera
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
 
@@ -35,12 +37,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         presentViewController(imagePicker, animated: true, completion: nil)
     }
+    
     // Pick an image from Camera
     @IBAction func pickImageFromCamera(sender: UIBarButtonItem) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
         self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    // UIImagePickerControllerDelegate method that sends the selected image to the image view
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            if let image = info[UIImagePickerControllerOriginalImage]as? UIImage {
+                self.imagePickerView.image = image
+            }
+            self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func cancelAction(sender: UIBarButtonItem) {
