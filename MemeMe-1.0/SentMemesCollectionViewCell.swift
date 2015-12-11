b7a841c919f1eb7534d6360af2b6f1e016b718d8
Chris Garvey <chrisgarvey@chris-garveys-imac.local>
2015-12-11 14:10:02 -0800

    create collection view functionality

---
+++ MemeMe-1.0/SentMemesCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe-2.0
+//
+//  Created by Chris Garvey on 12/11/15.
+//  Copyright © 2015 Chris Garvey. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sentMemesImageView: UIImageView!
+    
+    
+}
---
+++ MemeMe-1.0/SentMemesCollectionViewController.swift
@@ -0,0 +1,99 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe-2.0
+//
+//  Created by Chris Garvey on 12/11/15.
+//  Copyright © 2015 Chris Garvey. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "SentMemesCollectionViewCell"
+
+class SentMemesCollectionViewController: UICollectionViewController {
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    var memes: [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView!.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        // citation: https://discussions.udacity.com/t/my-uicollectionview-is-not-working-and-i-dont-know-why/14362/9
+        // Don't register because the reuse identifier has been entered via storyboard
+
+        // Do any additional setup after loading the view.
+        let space: CGFloat = 3.0
+        let dimension = (self.view.frame.size.width - (2 * space)) / space
+        print(dimension)
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! SentMemesCollectionViewCell
+        cell.sentMemesImageView?.image = memes[indexPath.row].memedImage
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
